1ebf7ba279cc2b1c3db8c6d703f9bbe44d362778
JeongMin Woo <minhan_a@naver.com>
2018-11-06 13:24:17 +0900

    saga-beginner finish

--- redux-saga-beginner-tutorial-master/sagas.js
+++ redux-saga-beginner-tutorial-master/sagas.js
@@ -1,5 +1,10 @@
 import {delay} from 'redux-saga'
-import {put, takeEvery} from 'redux-saga/effects'
+import {put, takeEvery, all} from 'redux-saga/effects'
+
+
+export function* helloSaga() {
+  console.log('Hello Sagas!')
+}
 
 //worker saga : 비동지 증가 태스크 수행
 export function* incrementAsync() {
@@ -10,4 +15,11 @@ export function* incrementAsync() {
 //watcher Saga : 각각의 INCREMENT_ASYNC 에 incrementAsync 테스크를 생성
 export function* watchIncrementAsync() {
   yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+}
+
+export default function* rootSaga() {
+  yield all([
+    helloSaga(),
+    watchIncrementAsync()
+  ])
 }
