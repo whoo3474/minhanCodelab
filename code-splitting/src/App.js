9d6996cef687264981ad00886fea6370b455874c
JeongMin Woo <minhan_a@naver.com>
2018-12-03 18:17:36 +0900

    code-splitting : code-splitting SplitMe.js in src/App.js

--- code-splitting/src/App.js
+++ code-splitting/src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  handleClick = () => {
+  state = {
+    SplitMe: null,
+    toggle : false
+  };
+  alertHandleClick = () => {
     import('./notify').then(({default:notify}) =>{
       notify();
     })
   };
+  componentHandleClick = () => {
+    import('./SplitMe').then(({ default: SplitMe }) => {
+      this.setState({
+        SplitMe,
+        toggle : this.state.toggle?false:true
+      });
+    });
+  };
   render() {
+    const { SplitMe,toggle } = this.state;
     return (
       <div>
-        <button onClick={this.handleClick}>Click Me</button>
+      <button onClick={this.alertHandleClick}>Click Me-alert</button>
+        <button onClick={this.componentHandleClick}>Click Me-component</button>
+        {toggle ? SplitMe && <SplitMe /> : ''}
       </div>
     );
   }
