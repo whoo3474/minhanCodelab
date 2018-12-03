9e6da4963c206c8409f16ec4d2f4a6eabe2b9aaa
JeongMin Woo <minhan_a@naver.com>
2018-12-03 18:33:53 +0900

    code-splitting : apply react-router in App.js

--- code-splitting/src/App.js
+++ code-splitting/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import withSplitting from './withSplitting'
+import { Route, Link } from 'react-router-dom';
+import { About, Home } from './pages';
+import withSplitting from './withSplitting';
 const SplitMe_2 = withSplitting(() => import('./SplitMe_2'));
 
 class App extends Component {
@@ -30,11 +32,30 @@ class App extends Component {
     const { SplitMe,toggle,visible  } = this.state;
     return (
       <div>
-      <button onClick={this.alertHandleClick}>Click Me-alert</button>
-      <button onClick={this.componentHandleClick}>Click Me-component</button>
-      <button onClick={this.handleClick}>Click Me-HOC code-splitting</button>
-      {visible && <SplitMe_2 />}
-      {toggle ? SplitMe && <SplitMe /> : ''}
+        <button onClick={this.alertHandleClick}>Click Me-alert</button>
+        <button onClick={this.componentHandleClick}>Click Me-component</button>
+        <button onClick={this.handleClick}>Click Me-HOC code-splitting</button>
+        {visible && <SplitMe_2 />}
+        {toggle ? SplitMe && <SplitMe /> : ''}
+
+        <hr/>
+
+        <ul>
+          <li>
+            <Link to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="about">
+              About
+            </Link>
+          </li>
+        </ul>
+        
+        <hr />
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
       </div>
     );
   }
